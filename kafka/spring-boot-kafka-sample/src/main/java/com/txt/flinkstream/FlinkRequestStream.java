5070e8b4e536e7ad04c7d288a4a8bec156efde3e
ThuongTX <trinhxuan92@gmail.com>
2024-04-10 22:58:22 +0700

    spring-boot-kafka-sample: update FlinkRequestStream example

--- kafka/spring-boot-kafka-sample/src/main/java/com/txt/flinkstream/FlinkRequestStream.java
+++ kafka/spring-boot-kafka-sample/src/main/java/com/txt/flinkstream/FlinkRequestStream.java
@@ -23,33 +23,28 @@ import java.util.UUID;
 public class FlinkRequestStream {
     private static final Logger logger = LoggerFactory.getLogger(FlinkRequestStream.class);
 
-    private static String inputTopic = "flink_stream_input";
-    private static String outputTopic = "flink_stream_output";
-    private static String consumerGroup = "flink_stream_group";
-    private static String address = "localhost:9092";
+    private static String inputTopic = "flink_input";
+    private static String outputTopic = "flink_output";
+    private static String groupId = "flink_group";
+    private static String addressKafka = "localhost:9092";
 
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        final String kafkaBootStrapServer = address;
-        final String groupId = consumerGroup;
-        logger.info("Kafka bootstrap servers: {}, Group id: {}", kafkaBootStrapServer, groupId);
+        logger.info("Kafka bootstrap servers: {}, Group id: {}", addressKafka, groupId);
         Properties kafkaProps = new Properties();
-        kafkaProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootStrapServer);
+        kafkaProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, addressKafka);
         kafkaProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
 
-        // create a kafka consumer to topic workflow bo in
-        final String bizObjInTopic = inputTopic;
-        FlinkKafkaConsumer<String> bizObjFlinkKafkaConsumer = new FlinkKafkaConsumer<>(bizObjInTopic, new SimpleStringSchema(), kafkaProps);
-        //bizObjFlinkKafkaConsumer.setStartFromGroupOffsets();
+        FlinkKafkaConsumer<String> bizObjFlinkKafkaConsumer = new FlinkKafkaConsumer<>(inputTopic, new SimpleStringSchema(), kafkaProps);
         bizObjFlinkKafkaConsumer.setStartFromEarliest();
-        logger.info("Biz obj log topic: {}", bizObjInTopic);
+        logger.info("Obj log topic: {}", inputTopic);
 
         // jackson
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         // get data from topic in
-        DataStream<String> bizObjStream = env.addSource(bizObjFlinkKafkaConsumer).name("Data from Kafka " + bizObjInTopic + " - " + kafkaBootStrapServer);
+        DataStream<String> bizObjStream = env.addSource(bizObjFlinkKafkaConsumer).name("Data from Kafka " + inputTopic + " - " + addressKafka);
         bizObjStream.rebalance();
 
         DataStream<TransactionDTO> bizObjResultStream = bizObjStream.map(new MapFunction<>() {
@@ -64,36 +59,23 @@ public class FlinkRequestStream {
         });
 
         // create a Kafka producer to topic out
-        final String bizObjOutTopic = outputTopic;
         final int kafkaTransactionTimeout = 300000;
         kafkaProps.put("transaction.timeout.ms", kafkaTransactionTimeout);
-        logger.info("Biz obj out topic: {}, Kafka transaction timeout: {}", bizObjOutTopic, kafkaTransactionTimeout);
-        FlinkKafkaProducer<TransactionDTO> bizObjResultFlinkKafkaProducer = new FlinkKafkaProducer<TransactionDTO>(bizObjOutTopic, new KafkaSerializationSchema<TransactionDTO>() {
+        logger.info("Oj out topic: {}, Kafka transaction timeout: {}", outputTopic, kafkaTransactionTimeout);
+        FlinkKafkaProducer<TransactionDTO> kafkaProducer = new FlinkKafkaProducer<TransactionDTO>(outputTopic, new KafkaSerializationSchema<TransactionDTO>() {
             @SneakyThrows
             @Override
             public ProducerRecord<byte[], byte[]> serialize(TransactionDTO transactionOps, Long aLong) {
                 logger.info("TransactionDTO SEND TO KAFKA: {}", transactionOps);
-                return new ProducerRecord<byte[], byte[]>(bizObjOutTopic, objectMapper.writeValueAsString(transactionOps).getBytes(StandardCharsets.UTF_8));
+                return new ProducerRecord<byte[], byte[]>(outputTopic, objectMapper.writeValueAsString(transactionOps).getBytes(StandardCharsets.UTF_8));
             }
         }, kafkaProps, FlinkKafkaProducer.Semantic.EXACTLY_ONCE);
 
         // versions 0.10+ allow attaching the records' event timestamp when writing them to Kafka;
         // this method is not available for earlier Kafka versions
-        bizObjResultFlinkKafkaProducer.setWriteTimestampToKafka(true);
+        kafkaProducer.setWriteTimestampToKafka(true);
 
-        bizObjResultStream.addSink(bizObjResultFlinkKafkaProducer).name("Data to Kafka " + bizObjOutTopic + " - " + kafkaBootStrapServer);
-
-        // create a Kafka producer to topic log
-		/* final String bizObjLogTopic = (null == parameter ? "BO-LOG" : parameter.get("kafka.wf.topic.log", "BO-LOG"));
-		logger.info("Biz obj log topic {}, Kafka transaction timeout {}", bizObjLogTopic, kafkaTransactionTimeout);
-		FlinkKafkaProducer<ResponseResult> bizObjResultLogFlinkKafkaProducer = new FlinkKafkaProducer<ResponseResult>(bizObjLogTopic,
-				new KafkaProducerLogSerializationSchema(bizObjLogTopic, objectMapper), kafkaProps,
-				FlinkKafkaProducer.Semantic.EXACTLY_ONCE); */
-
-        // versions 0.10+ allow attaching the records' event timestamp when writing them to Kafka;
-        // this method is not available for earlier Kafka versions
-		/* bizObjResultLogFlinkKafkaProducer.setWriteTimestampToKafka(true);
-		bizObjResultStream.addSink(bizObjResultLogFlinkKafkaProducer).name("Data to Kafka " + bizObjLogTopic + " - " + kafkaBootStrapServer); */
+        bizObjResultStream.addSink(kafkaProducer).name("Data to Kafka " + outputTopic + " - " + addressKafka);
 
         env.execute("Data Request Stream");
     }
